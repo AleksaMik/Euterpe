a27156dc3e14d16a7526dd3698ce2c4119c42654
AleksaMik <aleksam.2509@gmail.com>
2022-08-30 17:18:53 -0500

    function runSearch created

--- index.js
+++ index.js
@@ -14,4 +14,48 @@ connection.connect ((err) => {
     if(err) throw err;
     runSearch();
 });
-
+function runSearch() {
+    inquiirer
+    .prompt({
+        name: "selection",
+        type:"list",
+        message: "What would you like to do?",
+        choices:
+        [
+            "View All Employees",
+            "View Department",
+            "View role",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Role",
+        ]
+    })
+    .then(function(answer){
+        console.log(answer);
+        if (answer.selection === "View All Employees") {
+            viewAll();
+        }
+        else if (answer.selection ===  "View Department") {
+            viewdepts();
+        }
+        else if (answer.selection === "View role") {
+            viewRole();
+        }
+        else if (answer.selection === "Add Employee") {
+            addEmployee();
+        }
+        else if (answer.selection === "Add Department") {
+            addDept();
+        }
+        else if (answer.selection === "Add Role") {
+            addRole();
+        }
+        else if (answer.selection ===  "Update Role") {
+            updateRole();
+        } 
+        else {
+            connection.end();
+        }
+    });
+}
