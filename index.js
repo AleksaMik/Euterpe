8d51830bd7d2811f4f2e9750758596a6dcf291ec
AleksaMik <aleksam.2509@gmail.com>
2022-08-30 17:07:47 -0500

    index.js var created

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log ('hello world');
+var inquirer = require ('inquirer');
+var mysql = require ('mysql');
+var cTable = require ('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port:3001,
+    user:"root",
+    password:" ",
+    database: "employee_tracker"
+});
+
+connection.connect ((err) => {
+    if(err) throw err;
+    runSearch();
+});
+
