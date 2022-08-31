1a2d49e336150ef6737f8dff208d0df9a71f95c0
AleksaMik <aleksam.2509@gmail.com>
2022-08-31 06:59:00 -0500

    schema.sql updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,46 +4,48 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
-    role_id INT,
-    manager_id INT, 
-    PRIMARY KEY (id),
-    FOREIGN KEY (manager_id)
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+    -- FOREIGN KEY(role_id)
+    -- REFERENCES role(id),
+    FOREIGN KEY(manager_id)
     REFERENCES employee(id)
+
 );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Ron", "Paul", 1, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Frederick", "Douglas", 2, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Tulsi", "Gabbard", 4, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Mos", "Def", 3, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Chester", "Bennington", 5, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Seth", "Gerlach", 2, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Gary", "Oldman", 3, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Harry", "Potter", 1, 1 );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Robert", "Pattinson", 1, 1);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Khris", "Smith", 1, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("John", "McGregor", 2, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Anna", "Davis", 3, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Kate", "Denis", 4, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Artur", "Johansen", 5, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Mary", "Davidson", 3, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Alex", "Ball", 2, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Peter", "Parker", 1, 1 );
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Sherlock", "Holmes", 2, 1);
 
-CRETAE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    PRIMARY KEY (id)
-    FOREIGN KEY(department_id),
-    REFERENCE DEPARTMENT(id)
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id)
+  -- FOREIGN KEY(department_id)
+  -- REFERENCES department(id)
 );
-
-insert into role(title, salary, department_id) values('Manager', 400000, 1);
-insert into role(title, salary, department_id) values('Engineer', 200000, 2);
-insert into role(title, salary, department_id) values('Accountant', 100000, 3);
-insert into role(title, salary, department_id) values('Designer', 80000, 4);
-insert into role(title, salary, department_id) values('Administration', 70000, 5);
+insert into role(title, salary, department_id) values('Manager', 5000000, 1);
+insert into role(title, salary, department_id) values('Engineer', 1000000, 2);
+insert into role(title, salary, department_id) values('Accountant', 4000000, 3);
+insert into role(title, salary, department_id) values('Designer', 3000000, 4);
+insert into role(title, salary, department_id) values('Administration', 2000000, 5);
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 insert into department(name)
 values ('Management'),
@@ -51,3 +53,4 @@ values ('Management'),
        ('Accounting'),
        ('Marketing'),
        ('Human Resources');
+
